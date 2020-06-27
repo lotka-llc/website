f372b422b694a36727df722d63905848cbd92faa
lotka-llc <lotka.corporation@gmail.com>
2020-06-27 13:40:31 +0430

    sxcfg

--- wp-content/themes/neve/dashboard/src/Components/Onboarding/store/selectors.js
+++
@@ -1,9 +0,0 @@
-export default {
-	getSites: (state) => state.sites,
-	getMigrationData: (state) => state.migrationData,
-	getCurrentEditor: (state) => state.editor,
-	getCurrentSite: (state) => state.currentSite,
-	getPreviewStatus: (state) => state.previewStatus,
-	getImportModalStatus: (state) => state.importModalStatus,
-	getOnboardingStatus: (state) => state.isOnboarding
-};
